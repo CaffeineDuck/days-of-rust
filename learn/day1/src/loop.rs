1ff72cc6fbb661b9d5f8aba322e59f1ca24d8bf9
Samrid Pandit <samrid.pandit@gmail.com>
2021-08-03 01:27:05 +0545

    Revert "[add] Creates `function.rs` with the proper info about loops in rust"
    This reverts commit 0ea6a7a66485de35375787e138101d4e2bb511fe.

--- learn/day1/src/loop.rs
+++
@@ -1,9 +0,0 @@
-// More about loops here: https://doc.rust-lang.org/book/ch03-05-control-flow.html
-
-fn main() {
-    // This can be used to do a never ending loop. 
-    // Press Ctrl+C to stop this never ending loop
-    loop {
-        println!("This is how you can do a never ending loop")
-    }
-}
