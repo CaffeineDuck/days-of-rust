127b6e24c3c4a5430944aa9d241cfb219b1f4145
Samrid Pandit <samrid.pandit@gmail.com>
2021-08-03 01:22:18 +0545

    [add] Creates `hello.rs` with the hello world program

---
+++ learn/day1/src/hello.rs
@@ -0,0 +1,7 @@
+// `main` function is the entrypoint to the program in rust.
+// It does not care about the position of the placement of the function, 
+// the function just needs to be there. 
+fn main() {
+    // println! is used to print to the console.
+    println!("Hello World!");
+}
